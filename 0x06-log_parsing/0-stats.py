73f0b377f7fd87b135cfea7a9c495e1b44bed46d
monicajoa <mnortiz.ortiz@gmail.com>
2021-03-11 20:30:15 -0500

    Log Parsing project, script that reads stdin line by line and computes metrics, add stact file again, module condition equal zero

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,31 +2,27 @@
 """ This module reads the stdind line by line
     and comupte metrics of the file size and status codes
 """
+
 import sys
+
 total_size = 0
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
 try:
     for log, line in enumerate(sys.stdin, 1):
-        try:
-            aux_line = line.split(' ')
-            st_code = aux_line[-2]
-            fl_size = aux_line[-1]
-            if st_code in status_codes:
-                status_codes[st_code] += 1
-            total_size += eval(fl_size)
-        except:
-            pass
-        if log % 10:
+        aux_line = line.split(" ")
+        if len(aux_line) < 2:
+            continue
+        st_code = aux_line[-2]
+        fl_size = aux_line[-1]
+        if st_code in status_codes:
+            status_codes[st_code] += 1
+        total_size += eval(fl_size)
+        if log % 10 == 0:
             print("File size: {}".format(total_size))
             for key, value in sorted(status_codes.items()):
                 if value > 0:
                     print("{}: {}".format(key, value))
-except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
 finally:
     print("File size: {}".format(total_size))
     for key, value in sorted(status_codes.items()):
