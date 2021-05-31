84e852e2eaf6bb4fdf523153cc3f72d59658a01d
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-30 23:50:40 -0500

    Add rain task, Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains, return the integer indicating total amount of rainwater retained

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module containing the rain task,
+    Calculate how many square units of water
+    will be retained after it rains on a relief map
+"""
+
+
+def rain(walls):
+    water_limit = 0
+    distance = 0
+    height = 0
+
+    for index in walls:
+        if index == 0 and height > 0:
+            distance += 1
+        if index > 0:
+            wall_height = min(height, index)
+            water_limit += wall_height * distance
+            height = index
+            distance = 0
+
+    return water_limit
