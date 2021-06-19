73b124544e22a15601d9c499b0f668d38a939452
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-19 18:57:03 -0500

    Add reviewed task, recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords, Invalid subreddits may return a redirect to search results

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -6,24 +6,24 @@
 """
 import requests
 
-rUrl = 'https://api.reddit.com/r/'
-hotUrl = '{}/hot.json?after={}'
+reddit_url = 'https://api.reddit.com/r/'
+aux_url = '{}/hot.json?after={}'
 
 
 def recurse(subreddit, new_list=[], after=None):
     """ Method that Get list
     """
     if after is not None:
-        url = rUrl + hotUrl.format(subreddit, after)
+        url = reddit_url + aux_url.format(subreddit, after)
     else:
-        url = rUrl + '{}/hot.json'.format(subreddit)
+        url = reddit_url + '{}/hot.json'.format(subreddit)
     header = {'user-agent': 'mr. big'}
 
-    res = requests.get(url, headers=header, allow_redirects=False)
-    if res.status_code != 200:
+    result = requests.get(url, headers=header, allow_redirects=False)
+    if result.status_code != 200:
         return None
     try:
-        resJson = res.json()
+        resJson = result.json()
     except Exception:
         return None
 
@@ -39,25 +39,25 @@ def recurse(subreddit, new_list=[], after=None):
         return new_list
 
 
-def print_words(word_list, hot_list):
+def print_new_words(word_list, new_list):
     """ Method that print words
     """
     new_dict = {}
     for word in word_list:
-        c = 0
+        x = 0
         for title in new_list:
-            t = title.lower().split()
-            if word.lower().strip() in t:
-                c += t.count(word.lower().strip())
+            j = title.lower().split()
+            if word.lower().strip() in j:
+                x += j.count(word.lower().strip())
 
         if word.lower() in new_dict:
-            new_dict[word.lower()] += c
+            new_dict[word.lower()] += x
         else:
-            new_dict.update({word.lower(): c})
+            new_dict.update({word.lower(): x})
 
-    for k, v in sorted(new_dict.items(), key=lambda x: (-x[1], x[0])):
-        if new_dict.get(k) != 0:
-            print("{}: {}".format(k, v))
+    for key, value in sorted(new_dict.items(), key=lambda x: (-x[1], x[0])):
+        if new_dict.get(key) != 0:
+            print("{}: {}".format(key, value))
 
 
 def count_words(subreddit, word_list):
@@ -67,4 +67,4 @@ def count_words(subreddit, word_list):
     if new_list is None:
         return
 
-    print_words(word_list, new_list)
+    print_new_words(word_list, new_list)
