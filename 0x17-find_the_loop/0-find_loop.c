77b4ecd1362ca14bc718538239815a121b6e7adb
monicajoa <mnortiz.ortiz@gmail.com>
2021-07-15 23:39:58 -0500

    Add Find the loop task, function that finds the loop in a linked list

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,14 +1,17 @@
 #include "lists.h"
+
 /**
  * find_listint_loop - Check if there is a cycle in a linked list
- * @head: linked list head
+ * @head: Linked list head
  *
- * Return: address of the pointer where the cycle is
+ * Return: Address of the pointer where the cycle is
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *temp = head;
 	listint_t *curr = head;
+
 	while (temp != NULL && temp->next != NULL)
 	{
 		curr = curr->next;
@@ -16,6 +19,12 @@ listint_t *find_listint_loop(listint_t *head)
 		temp = temp->next;
 		if (temp == curr)
 		{
+			curr = head;
+			while (curr != temp)
+			{
+				curr = curr->next;
+				temp = temp->next;
+			}
 			return (temp);
 		}
 	}
