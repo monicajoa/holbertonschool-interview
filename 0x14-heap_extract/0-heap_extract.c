b19624d09caf929b7602410fa83eb8ab528f7b28
monicajoa <mnortiz.ortiz@gmail.com>
2021-07-22 23:36:48 -0500

    Add  Heap - Extract task, function that extracts the root node of a Max Binary Heap, fix betty style

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -9,24 +9,25 @@
 
 void move_node(heap_t *node)
 {
-    heap_t *largest = NULL;
-    int aux;
-    if (node->right)
-    {
-        if (node->right->n > node->left->n)
-            largest = node->right;
-        else
-            largest = node->left;
-    }
-    else if (node->left)
-        largest = node->left;
-    if (largest && largest->n > node->n)
-    {
-        aux = largest->n;
-        largest->n = node->n;
-        node->n = aux;
-        move_node(largest);
-    }
+	heap_t *largest = NULL;
+	int aux;
+
+	if (node->right)
+	{
+		if (node->right->n > node->left->n)
+			largest = node->right;
+		else
+			largest = node->left;
+	}
+	else if (node->left)
+		largest = node->left;
+	if (largest && largest->n > node->n)
+	{
+		aux = largest->n;
+		largest->n = node->n;
+		node->n = aux;
+		move_node(largest);
+	}
 }
 
 /**
@@ -38,14 +39,15 @@ void move_node(heap_t *node)
 
 int get_height(heap_t *node)
 {
-    int l, r;
-    if (!node)
-        return (0);
-    l = get_height(node->left);
-    r = get_height(node->right);
-    if (l > r)
-        return (l + 1);
-    return (r + 1);
+	int l, r;
+
+	if (!node)
+		return (0);
+	l = get_height(node->left);
+	r = get_height(node->right);
+	if (l > r)
+		return (l + 1);
+	return (r + 1);
 }
 
 /**
@@ -59,18 +61,19 @@ int get_height(heap_t *node)
 
 heap_t *get_last_node(int height, int rec_height, heap_t *node)
 {
-    heap_t *left;
-    heap_t *right;
-    if (rec_height != height)
-    {
-        left = get_last_node(height, rec_height + 1, node->left);
-        right = get_last_node(height, rec_height + 1, node->right);
-    }
-    else
-        return (node);
-    if (right)
-        return (right);
-    return (left);
+	heap_t *left;
+	heap_t *right;
+
+	if (rec_height != height)
+	{
+		left = get_last_node(height, rec_height + 1, node->left);
+		right = get_last_node(height, rec_height + 1, node->right);
+	}
+	else
+		return (node);
+	if (right)
+		return (right);
+	return (left);
 }
 
 /**
@@ -81,9 +84,10 @@ heap_t *get_last_node(int height, int rec_height, heap_t *node)
 
 heap_t *get_last(heap_t *root)
 {
-    int height;
-    height = get_height(root);
-    return (get_last_node(height, 1, root));
+	int height;
+
+	height = get_height(root);
+	return (get_last_node(height, 1, root));
 }
 
 /**
@@ -94,25 +98,26 @@ heap_t *get_last(heap_t *root)
 
 int heap_extract(heap_t **root)
 {
-    heap_t *aux;
-    int swap;
-    if (!*root)
-        return (0);
-    if (!(*root)->left && !(*root)->right)
-    {
-        swap = (*root)->n;
-        free(*root);
-        *root = NULL;
-        return (swap);
-    }
-    aux = get_last(*root);
-    if (aux->parent->right)
-        aux->parent->right = NULL;
-    else
-        aux->parent->left = NULL;
-    swap = (*root)->n;
-    (*root)->n = aux->n;
-    move_node(*root);
-    free(aux);
-    return (swap);
+	heap_t *aux;
+	int swap;
+
+	if (!*root)
+		return (0);
+	if (!(*root)->left && !(*root)->right)
+	{
+		swap = (*root)->n;
+		free(*root);
+		*root = NULL;
+		return (swap);
+	}
+	aux = get_last(*root);
+	if (aux->parent->right)
+		aux->parent->right = NULL;
+	else
+		aux->parent->left = NULL;
+	swap = (*root)->n;
+	(*root)->n = aux->n;
+	move_node(*root);
+	free(aux);
+	return (swap);
 }
