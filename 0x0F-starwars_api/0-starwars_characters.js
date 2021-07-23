1862eeb081f664945dcc71500aa3917b97ca9895
monicajoa <mnortiz.ortiz@gmail.com>
2021-07-22 22:59:58 -0500

    Add Star Wars Characters task, script that prints all characters of a Star Wars movie

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-
 request(filmUrl, (error, response, body) => {
   if (error) {
     return;
   }
-
   const film = JSON.parse(body);
   const UrlName = {};
-
   film.characters.forEach(characterUrl => {
     request(characterUrl, (error, response, body) => {
       if (error) {
         return;
       }
-
       const character = JSON.parse(body);
       UrlName[character.url] = character.name;
       if (Object.keys(UrlName).length === film.characters.length) {
@@ -24,3 +20,4 @@ request(filmUrl, (error, response, body) => {
       }
     });
   });
+});
